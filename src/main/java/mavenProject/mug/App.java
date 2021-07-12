8c0a824a90a9ccb47077753738451ad81df18b9a
Luv Mendiratta <luv.mendiratta@solisconsulting.local>
2021-07-12 12:30:44 +0100

    adding file to remote

---
+++ src/main/java/mavenProject/mug/App.java
@@ -0,0 +1,30 @@
+package mavenProject.mug;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class App 
+{
+	public static void main( String[] args ) throws InterruptedException
+	{
+		System.out.println( "Hello World!" );
+		App app = new App();
+		app.operaload();
+	}
+
+	public void operaload() throws InterruptedException 
+	{
+		DesiredCapabilities capablities=DesiredCapabilities.opera();
+		String opera_profile = "C:\\Users\\luv.mendiratta\\AppData\\Roaming\\Opera Software\\Opera Stable";
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--user-data-dir="+opera_profile);
+		chromeOptions.setBinary("C:\\Users\\luv.mendiratta\\AppData\\Local\\Programs\\Opera\\77.0.4054.172\\opera.exe");
+		System.setProperty("webdriver.opera.driver", "C:\\Users\\luv.mendiratta\\Desktop\\operadriver_win64\\operadriver_win64\\operadriver.exe");
+		capablities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+		OperaDriver driver = new OperaDriver(capablities);
+		driver.get("https://whatismyipaddress.com");
+		Thread.sleep(1000);
+		driver.quit();
+	}
+}
